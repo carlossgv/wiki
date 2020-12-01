66810cd5bfdc16f58f8d3383d0823307357953a7
Carlos Gonzalez <carlossgv@gmail.com>
2020-11-30 23:13:01 -0300

    Finished Entry Page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+
+    info = util.get_entry(title)
+
+    if info == None: 
+        info = "File Not Found"
+
+    else:
+        info = markdown.markdown(info)
+        print(info)
+    
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "info": info
+    })
